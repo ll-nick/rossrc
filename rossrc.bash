d851304d19df4e99637068411592f578dee284f8
ll-nick <github@le-large.net>
2025-02-23 17:41:46 +0100

    Move get devel dir and get setup file into separate functions

--- rossrc.bash
+++ rossrc.bash
@@ -36,6 +36,27 @@ rossrc() {
         echo ""  # Return an empty string if no workspace is found
     }
 
+    get_devel_dir() {
+        local ws_root="$1"
+        local profile_file="$ws_root/.catkin_tools/profiles/profiles.yaml"
+        local devel_dir="devel"
+
+        if [ -f "$profile_file" ]; then
+            local active_profile
+            active_profile=$(sed 's/active: //' < "$profile_file")
+            if [ "$active_profile" != "release" ]; then
+                devel_dir="devel_$active_profile"
+            fi
+        fi
+        echo "$devel_dir"
+    }
+
+    get_setup_file() {
+        local ws_root="$1"
+        local devel_dir="$2"
+        echo "$ws_root/$devel_dir/setup.bash"
+    }
+
     # Source global ROS setup if not already sourced
     # TODO: Make sourcing the global env a configurable function to make this tool more generic
     # (e.g. if not global_env() then global_env_default())
@@ -43,8 +64,7 @@ rossrc() {
         source_global_env
     fi
 
-    # Not a catkin workspace if not in a directory with _ws in the name
-    # Replace with function:
+    # If heuristic is not satisfied, we can return early
     if ! is_within_workspace_heuristic "$(pwd)"; then
         return
     fi
@@ -59,19 +79,13 @@ rossrc() {
 
     # Determine the active profile
     # TODO: Make the devel dir function configurable
-    local profile_file="$ws_root/.catkin_tools/profiles/profiles.yaml"
-    local devel_dir="devel"
-    if [ -f "$profile_file" ]; then
-        local active_profile
-        active_profile=$(sed 's/active: //' < "$profile_file")
-        if [ "$active_profile" != "release" ]; then
-            devel_dir="devel_$active_profile"
-        fi
-    fi
+    local devel_dir
+    devel_dir=$(get_devel_dir "$ws_root")
 
     # Source the correct setup file
     # TODO: If the workspace changed, re-source the global setup (but not if only the profile changed)
-    local setup_file="$ws_root/$devel_dir/setup.bash"
+    local setup_file
+    setup_file=$(get_setup_file "$ws_root" "$devel_dir")
 
     # Avoid re-sourcing if already in the same workspace
     if [ "$ROS_SETUP_BASH" == "$setup_file" ]; then
