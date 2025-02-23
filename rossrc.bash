b69fa29f58fe5fcb385ed9647ad365a80adfd53a
ll-nick <github@le-large.net>
2025-02-23 17:41:46 +0100

    Add function to find workspace root. Rename some variables for clarity.

--- rossrc.bash
+++ rossrc.bash
@@ -8,19 +8,32 @@ rossrc() {
     }
 
     is_within_workspace_heuristic() {
-        local ws_dir="$1"
-        if [[ "$ws_dir" != *_ws* ]]; then
+        local dir="$1"
+        if [[ "$dir" != *_ws* ]]; then
             return 1
         fi
         return 0
     }
 
-    is_workspace() {
-        local ws_dir="$1"
-        if [ -d "$ws_dir/.catkin_tools" ]; then
+    is_workspace_root() {
+        local dir="$1"
+        if [ -d "$dir/.catkin_tools" ]; then
             return 0
         fi
         return 1
+    }
+
+    get_workspace_root() {
+        local dir="$1"
+        while [ "$dir" != "/" ]; do
+            if ! is_workspace_root "$dir"; then
+                dir=$(dirname "$dir")
+                continue
+            fi
+            echo "$dir"
+            return
+        done
+        echo ""  # Return an empty string if no workspace is found
     }
 
     # Source global ROS setup if not already sourced
@@ -30,53 +43,49 @@ rossrc() {
         source_global_env
     fi
 
-    ws_dir=$(pwd)
-
     # Not a catkin workspace if not in a directory with _ws in the name
     # Replace with function:
-    if ! is_within_workspace_heuristic "$ws_dir"; then
+    if ! is_within_workspace_heuristic "$(pwd)"; then
         return
     fi
 
     # Walk up the directory tree to find workspace root
     # TODO: Remove while loop, just cut path after _ws
-    while [ "$ws_dir" != "/" ]; do
-        if ! is_workspace "$ws_dir"; then
-            ws_dir=$(dirname "$ws_dir")
-            continue
-        fi
-
-        # Determine the active profile
-        # TODO: Make the devel dir function configurable
-        local profile_file="$ws_dir/.catkin_tools/profiles/profiles.yaml"
-        local devel_dir="devel"
-        if [ -f "$profile_file" ]; then
-            local active_profile
-            active_profile=$(sed 's/active: //' < "$profile_file")
-            if [ "$active_profile" != "release" ]; then
-                devel_dir="devel_$active_profile"
-            fi
-        fi
-
-        # Source the correct setup file
-        # TODO: If the workspace changed, re-source the global setup (but not if only the profile changed)
-        local setup_file="$ws_dir/$devel_dir/setup.bash"
-
-        # Avoid re-sourcing if already in the same workspace
-        if [ "$ROS_SETUP_BASH" == "$setup_file" ]; then
-            echo "Aldready sourced workspace: $ws_dir (profile: $active_profile)"
-            return
-        fi
-
-        # Store the new workspace path
-        export ROS_SETUP_BASH="$setup_file"
-        export ROS_WORKSPACE="$ws_dir"
-        if [ -f "$setup_file" ]; then
-            echo "Sourcing workspace: $setup_file (profile: $active_profile)"
-            source "$setup_file"
-        else
-            echo "No valid setup.bash found for profile ($active_profile)."
-        fi
+    local ws_root
+    ws_root=$(get_workspace_root "$(pwd)")
+    if [ -z "$ws_root" ]; then
         return
-    done
+    fi
+
+    # Determine the active profile
+    # TODO: Make the devel dir function configurable
+    local profile_file="$ws_root/.catkin_tools/profiles/profiles.yaml"
+    local devel_dir="devel"
+    if [ -f "$profile_file" ]; then
+        local active_profile
+        active_profile=$(sed 's/active: //' < "$profile_file")
+        if [ "$active_profile" != "release" ]; then
+            devel_dir="devel_$active_profile"
+        fi
+    fi
+
+    # Source the correct setup file
+    # TODO: If the workspace changed, re-source the global setup (but not if only the profile changed)
+    local setup_file="$ws_root/$devel_dir/setup.bash"
+
+    # Avoid re-sourcing if already in the same workspace
+    if [ "$ROS_SETUP_BASH" == "$setup_file" ]; then
+        echo "Aldready sourced workspace: $ws_root (profile: $active_profile)"
+        return
+    fi
+
+    # Store the new workspace path
+    export ROS_SETUP_BASH="$setup_file"
+    export ROS_WORKSPACE="$ws_root"
+    if [ -f "$setup_file" ]; then
+        echo "Sourcing workspace: $setup_file (profile: $active_profile)"
+        source "$setup_file"
+    else
+        echo "No valid setup.bash found for profile ($active_profile)."
+    fi
 }
